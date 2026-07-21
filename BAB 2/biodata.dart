3aa77c85b2513fb8229f18a5f1841faed002bb3b
Hideaki <muhammadhideakiisdianto@gmail.com>
2026-07-21 08:58:11 +0700

    first commit

---
+++ BAB 2/biodata.dart
@@ -0,0 +1,3 @@
+void main(){
+  print("Halo Nama Aku adalah Muhammad Hideaki Isdianto aku berasal dari kelas 11 RPL");
+}
---
+++ BAB 3/bab3.dart
@@ -0,0 +1,10 @@
+// ini adalah komentar satu baris
+/* ini adalah komentar 
+multi baris */
+void main(){
+  print("Cuman baris ini yang akan di eksekusi"); //saya tidak dieksekusi karena ini adalah komentar
+ const double pi = 3.14;
+  final double r = 7;
+  final double luas = pi * r * r;
+  print("Luas Lingkaran dengan jari-jari $r adalah $luas");
+}
