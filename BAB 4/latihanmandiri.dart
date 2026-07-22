d91ce3fe935698fd712a6de2a27c4db6ba996466
Hideaki <muhammadhideakiisdianto@gmail.com>
2026-07-22 09:18:34 +0700

    latihan bab 4

---
+++ BAB 4/latihanmandiri.dart
@@ -0,0 +1,6 @@
+void main(){
+  int a = 9;
+  for (int i = 1; i <=10; i++){
+    print('$a x $i = ${a * i}');
+  }
+}
--- latihanmandiri.dart
+++
@@ -1,6 +0,0 @@
-void main(){
-  int a = 9;
-  for (int i = 1; i <=10; i++){
-    print('$a x $i = ${a * i}');
-  }
-}
