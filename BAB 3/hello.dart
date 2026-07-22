14d1c353006222af997724ecca31bec0815cac64
Hideaki <muhammadhideakiisdianto@gmail.com>
2026-07-22 08:11:55 +0700

    tugas boy

---
+++ BAB 3/hello.dart
@@ -0,0 +1,11 @@
+void main(){
+  String nama = "Deki";
+  int umur = 16;
+  double tinggi = 1.78;
+  bool sudahlulus = false;
+
+  print('Nama: $nama');
+  print('Umur: $umur');
+  print('Tinggi: $tinggi');
+  print('Sudah Lulus: $sudahlulus');
+}
---
+++ BAB 3/variable.dart
@@ -0,0 +1,14 @@
+void main(){
+  var alamat = "Di rumah makan";
+  alamat = "di rumah sana";
+
+  final nisn = 012392232;
+  
+
+  const pi = 3.14;
+   
+
+  print(alamat);
+  print(nisn);
+  print(pi);
+}
